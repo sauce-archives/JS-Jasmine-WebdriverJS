80516ae701c86067b134a81e01d688d749efb75e
Leo Laskin <llaskin@gmail.com>
2016-08-08 18:57:47 -0500

    this works so far, let's keep digging at DriverFactory

--- tests/sampleSpec.js
+++ tests/sampleSpec.js
@@ -1,4 +1,5 @@
 var webdriver = require('selenium-webdriver');
+var config = require('config');
 var SauceLabs = require("saucelabs");
 var username = process.env.SAUCE_USERNAME;
 var accessKey = process.env.SAUCE_ACCESS_KEY;
@@ -13,40 +14,47 @@ describe('basic test', function () {
 
 	beforeEach(function () {
 		var browser = process.env.BROWSER,
-        version = process.env.VERSION,
-        platform = process.env.PLATFORM,
-        server = "http://" + username + ":" + accessKey +
-        "@ondemand.saucelabs.com:80/wd/hub";
+			version = process.env.VERSION,
+			platform = process.env.PLATFORM,
+			server = "http://" + username + ":" + accessKey +
+				"@ondemand.saucelabs.com:80/wd/hub";
 
 		driver = new webdriver.Builder().
-		    withCapabilities({
-		        'browserName': browser,
-		        'platform': platform,
-		        'version': version,
-		        'username': username,
-		        'accessKey': accessKey,
-		        'name': jasmine.getEnv().currentSpec.description
-		    }).
-		    usingServer(server).
-		    build();
+		withCapabilities({
+			'browserName': browser,
+			'platform': platform,
+			'version': version,
+			'username': username,
+			'accessKey': accessKey,
+			'name': jasmine.getEnv().currentSpec.description
+		}).
+		usingServer(server).
+		build();
 
 		driver.getSession().then(function(sessionid) {
-		    driver.sessionID = sessionid.id_;
+			driver.sessionID = sessionid.id_;
 		});
 	});
 
 	afterEach(function () {
 		var results = jasmine.getEnv().currentSpec.results_.failedCount;
 		saucelabs.updateJob(driver.sessionID, {
-      		passed: results === 0
-    	}, function () {});
+			passed: results === 0
+		}, function () {});
 		driver.quit();
 	});
 
 	it('should be on correct page', function (done) {
-		driver.get('https://saucelabs.com/test/guinea-pig');
+		driver.get(config.get("url"));
 		driver.getTitle().then(function(title) {
-			expect(title).toBe('I am a page title - Sauce Labs');
+			expect(title).toBe("title");
+			done();
+		});
+	});
+	it('should be on correct page2', function (done) {
+		driver.get(config.get("url"));
+		driver.getTitle().then(function(title) {
+			expect(title).toBe("I am a page title - Sauce Labs");
 			done();
 		});
 	});
